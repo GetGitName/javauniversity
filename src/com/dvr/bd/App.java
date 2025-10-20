4a53eecead39cef374582e5a7cd405f62d4bad8c
daniel.valdez2 <daniel.valdez2@bbva.com>
2025-10-19 19:57:05 -0600

    feat(app bd): se crea la clase App
    Se agrega la clasa App para el ejercicio de Base de Datos.

---
+++ src/com/dvr/bd/App.java
@@ -0,0 +1,14 @@
+package com.dvr.bd;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MB91491
+ */
+public class App {
+    
+}
